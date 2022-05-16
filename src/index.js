1139725e06d6fea7f98723f092ffa656f4b07a43
Mateusz Woźniak <matisiek11@gmail.com>
2022-05-16 23:10:31 +0200

    Added mongo watching

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const totp = require("totp-generator");
 const userAgent = require('user-agents');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha')
+
+const {MongoClient} = require('mongodb');
 const e = require("express");
 puppeteer.use(StealthPlugin());
 puppeteer.use(
@@ -66,7 +68,7 @@ async function login() {
     await page.mouse.click(340, 411);
 }
 
-async function payout(req, res) {
+async function payout(recipientOriginal, value) {
     try {
         browser = await puppeteer.launch({
             headless: false,
@@ -75,11 +77,7 @@ async function payout(req, res) {
             },
             args: ["--disable-gpu", "--disable-dev-shm-usage", "--disable-setuid-sandbox", "--no-sandbox", "--window-size=800,1400"]
         });
-        if (req.params.key !== '4112') return res.json({error: 'invalid_key'});
-        let recipient = req.params.recipient;
-        const recipientOriginal = recipient;
-        const value = req.params.value;
-        res.json({success: true, recipient, value});
+        let recipient = recipientOriginal;
         await login();
         const page = await browser.newPage();
         await page.waitForTimeout(5000);
@@ -102,16 +100,16 @@ async function payout(req, res) {
         } catch (err) {
         }
         await page.waitForTimeout(1500);
-        await page.evaluate(() => {
-            window.scrollBy(0, window.innerHeight);
-        });
+        // await page.evaluate(() => {
+        //     window.scrollBy(0, window.innerHeight);
+        // });
         await page.waitForTimeout(100);
         await page.keyboard.press('Enter');
         await page.waitForTimeout(3000);
         await page.screenshot({path: 'example.png'});
         let success = false;
         try {
-            await page.waitForXPath(`//*[contains(text(), "Poinformujemy użytkownika ${recipientOriginal}")]`, {timeout: 10000});
+            await page.waitForXPath(`//*[contains(text(), "Poinformujemy użytkownika ${recipientOriginal}")]`, {timeout: 30000});
             success = true;
         } catch (err) {
         }
@@ -121,20 +119,48 @@ async function payout(req, res) {
             consola.error(`Payout failed`)
         await page.goto('https://www.paypal.com/bizcomponents/logout');
         await browser.close();
+        return success;
     } catch (err) {
         console.log(err);
         consola.error('Payout failed due to browser exception');
+        return false;
     }
 }
 
-async function main() {
+async function listen() {
     const app = express();
-    app.all('/payout/:recipient/:value/:key', payout);
     app.get('/totp', (req, res) => (res.json({code: totp(key)})));
     const port = process.env.PORT || 4110;
     await app.listen(port);
     consola.success(`Listening on port: ${port}`);
 }
 
-main();
-// payout(null, null);
+async function watch() {
+    listen();
+    const client = await MongoClient.connect('mongodb://root:4NC2h4fQsUFtJmyt@srv1.paysafe.money', {useNewUrlParser: true});
+    const db = client.db('psc_exchange');
+    const coll = db.collection('exchanges');
+    while (true) {
+        const exchange = await coll.findOne({
+            payout_method: 'paypal',
+            payout_status: 'pending',
+            payment_status: 'valid'
+        });
+        if (exchange) {
+            console.log(exchange);
+            exchange.payout_status = 'automated_payout_working';
+            await coll.updateOne({_id: exchange._id}, {$set: exchange});
+            consola.info(`Sending ${exchange.effective_value} PLN to ${exchange.paypalEmail}`);
+            const success = await payout(exchange.paypalEmail, (Math.round(exchange.effective_value * 100) / 100).toFixed(2));
+            exchange.payout_status = success ? 'success' : 'automated_payout_needs_marked';
+            await coll.updateOne({_id: exchange._id}, {$set: exchange});
+        }
+        await delay(3000);
+    }
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+watch();
